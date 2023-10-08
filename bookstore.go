d688c49184d61a568f74070f0e8916e06cfd7cb3
Lucas <lucas.betancourt@hotmail.com>
2023-10-08 09:44:19 -0500

    Reworked functions, removed TestPrint funcs and added GetAllBooks, GetBookDetails and GetAllBookDetails and respective test functions.

--- bookstore.go
+++ bookstore.go
@@ -2,10 +2,11 @@ package bookstore
 
 import (
 	"fmt"
-	"io"
+	"strings"
 )
 
 type Book struct {
+	ID              string
 	Title           string
 	Authors         []string
 	Description     string
@@ -15,19 +16,68 @@ type Book struct {
 	Series          bool
 }
 
-func PrintBook(w io.Writer, book Book) {
-	fmt.Fprintf(w, "%+v\n", book)
+var Books = []Book{
+	{
+		ID:              "Book01",
+		Title:           "Spark Joy",
+		Authors:         []string{"Marie Kondo"},
+		Description:     "A tiny, cheerful Japanese woman explains tidying.",
+		Copies:          66,
+		PriceCents:      1199,
+		DiscountPercent: 10,
+		Series:          false,
+	},
+	{
+		ID:              "Book02",
+		Title:           "Death",
+		Authors:         []string{"Richard Beliveau"},
+		Description:     "A deep dive into the mysteries of life and the inevitable reality of death.",
+		Copies:          9,
+		PriceCents:      2999,
+		DiscountPercent: 10,
+		Series:          false,
+	},
+	{
+		ID:              "Book03",
+		Title:           "Lord of the rings",
+		Authors:         []string{"J.R.R Tolkien"},
+		Description:     "An epic fantasy novel that chronicles the adventures of hobbits, elves, and men against the dark lord Sauron.",
+		Copies:          20,
+		PriceCents:      1950,
+		DiscountPercent: 5,
+		Series:          true,
+	},
 }
 
-func NetPrice(priceCents, discountPercent int) int {
+func GetBookDetails(ID string) string {
+	for _, book := range Books {
+		if book.ID == ID {
+			authors := strings.Join(book.Authors, " ")
+			return fmt.Sprintf("%s by %s - %s\n", book.Title, authors, book.Description)
+		}
+	}
+	return ""
+}
+
+func ApplyDiscount(priceCents, discountPercent int) int {
 	return (priceCents * (100 - discountPercent)) / 100
 }
 
 func AddBook(books []Book, newBook Book) []Book {
 	books = append(books, newBook)
-	for i, b := range books {
-		fmt.Println(i, ": ", b.Title)
-	}
-	fmt.Println("Total books after adding new book is:", len(books))
 	return books
+}
+
+func GetAllBooks() []Book {
+	return Books
+}
+
+func GetAllBookDetails() string {
+	books := GetAllBooks()
+	var sb strings.Builder
+
+	for _, book := range books {
+		fmt.Fprint(&sb, GetBookDetails(book.ID))
+	}
+	return sb.String()
 }
--- bookstore_test.go
+++ bookstore_test.go
@@ -2,12 +2,14 @@ package bookstore_test
 
 import (
 	"bookstore"
-	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestBook(t *testing.T) {
 	_ = bookstore.Book{
+		ID:              "Book01",
 		Title:           "Spark Joy",
 		Authors:         []string{"Marie Kondo"},
 		Description:     "A tiny, cheerful Japanese woman explains tidying.",
@@ -18,10 +20,10 @@ func TestBook(t *testing.T) {
 	}
 }
 
-func TestPrintBook(t *testing.T) {
+func TestApplyDiscount(t *testing.T) {
 	t.Parallel()
-	var sb strings.Builder
 	book := bookstore.Book{
+		ID:              "Book02",
 		Title:           "Death",
 		Authors:         []string{"Richard Beliveau"},
 		Description:     "A deep dive into the mysteries of life and the inevitable reality of death.",
@@ -31,113 +33,65 @@ func TestPrintBook(t *testing.T) {
 		Series:          false,
 	}
 
-	bookstore.PrintBook(&sb, book)
-
-	expectedOutput := "{Title:Death Authors:[Richard Beliveau] Description:A deep dive into the mysteries of life and the inevitable reality of death. Copies:5 PriceCents:2999 DiscountPercent:10 Series:false}\n"
-
-	if sb.String() != expectedOutput {
-		t.Errorf("Expected %q, but got %q", expectedOutput, sb.String())
-	}
-}
-
-func TestNetPrice(t *testing.T) {
-	t.Parallel()
-	var sb strings.Builder
-	book := bookstore.Book{
-		Title:           "Death",
-		Authors:         []string{"Richard Beliveau"},
-		Description:     "A deep dive into the mysteries of life and the inevitable reality of death.",
-		Copies:          5,
-		PriceCents:      2999,
-		DiscountPercent: 10,
-		Series:          false,
-	}
-
-	bookstore.PrintBook(&sb, book)
-	// With the values above it should be 2699
-	discountedPrice := bookstore.NetPrice(book.PriceCents, book.DiscountPercent)
+	// In this example with the values above it should be 2699
+	expectedPrice := (book.PriceCents * (100 - book.DiscountPercent)) / 100
+	// Apply the discount
+	discountedPrice := bookstore.ApplyDiscount(book.PriceCents, book.DiscountPercent)
 	// update priceCents and DiscountPercent to reflect applied discount.
 	book.PriceCents = discountedPrice
 	book.DiscountPercent = 0
 
-	expectedPrice := 2699
 	if discountedPrice != expectedPrice {
 		t.Errorf("Expected price: %d, but got %d", expectedPrice, discountedPrice)
-	}
-
-	bookstore.PrintBook(&sb, book)
-}
-
-func TestPrintMultipleBooks(t *testing.T) {
-	t.Parallel()
-	var sb strings.Builder
-
-	books := []bookstore.Book{
-		{
-			Title:           "Death",
-			Authors:         []string{"Richard Beliveau"},
-			Description:     "A deep dive into the mysteries of life and the inevitable reality of death.",
-			Copies:          9,
-			PriceCents:      2999,
-			DiscountPercent: 10,
-			Series:          false,
-		},
-		{
-			Title:           "Lord of the rings",
-			Authors:         []string{"J.R.R Tolkien"},
-			Description:     "An epic fantasy novel that chronicles the adventures of hobbits, elves, and men against the dark lord Sauron.",
-			Copies:          20,
-			PriceCents:      1950,
-			DiscountPercent: 5,
-			Series:          true,
-		},
-		{
-			Title:           "100 years of solitude",
-			Authors:         []string{"Gabriel Garcia Marquez"},
-			Description:     "A magical realist novel that tells the story of the Buendía family in the fictional town of Macondo.",
-			Copies:          3,
-			PriceCents:      3500,
-			DiscountPercent: 0,
-			Series:          false,
-		},
-		{
-			Title:           "The Elements of Style",
-			Authors:         []string{"Strunk", "&", "White"},
-			Description:     "A prescriptive American English writing style guide that provides timeless advice on clarity, simplicity, and usage.",
-			Copies:          4,
-			PriceCents:      4999,
-			DiscountPercent: 5,
-			Series:          false,
-		},
-	}
-
-	for _, book := range books {
-		bookstore.PrintBook(&sb, book)
-	}
-
-	expectedOutput := "{Title:Death Authors:[Richard Beliveau] Description:A deep dive into the mysteries of life and the inevitable reality of death. Copies:9 PriceCents:2999 DiscountPercent:10 Series:false}\n" +
-		"{Title:Lord of the rings Authors:[J.R.R Tolkien] Description:An epic fantasy novel that chronicles the adventures of hobbits, elves, and men against the dark lord Sauron. Copies:20 PriceCents:1950 DiscountPercent:5 Series:true}\n" +
-		"{Title:100 years of solitude Authors:[Gabriel Garcia Marquez] Description:A magical realist novel that tells the story of the Buendía family in the fictional town of Macondo. Copies:3 PriceCents:3500 DiscountPercent:0 Series:false}\n" +
-		"{Title:The Elements of Style Authors:[Strunk & White] Description:A prescriptive American English writing style guide that provides timeless advice on clarity, simplicity, and usage. Copies:4 PriceCents:4999 DiscountPercent:5 Series:false}\n"
-
-	if sb.String() != expectedOutput {
-		t.Errorf("Expected output: %q, but got %q", expectedOutput, sb.String())
 	}
 }
 
 func TestAddBook(t *testing.T) {
 	t.Parallel()
 
-	books := []bookstore.Book{
-		{Title: "Death", Authors: []string{"Richard Beliveau"}, Copies: 9, PriceCents: 2999, DiscountPercent: 10, Series: false},
-		{Title: "Lord of the rings", Authors: []string{"J.R.R Tolkien"}, Copies: 20, PriceCents: 1950, DiscountPercent: 5, Series: true},
-	}
+	books := bookstore.Books
 
-	newBook := bookstore.Book{Title: "The Elements of Style", Authors: []string{"Strunk", "&", "White"}, Copies: 4, PriceCents: 4999, DiscountPercent: 5, Series: false}
+	newBook := bookstore.Book{ID: "Book04", Title: "The Elements of Style", Authors: []string{"Strunk", "&", "White"}, Copies: 4, PriceCents: 4999, DiscountPercent: 5, Series: false}
 
 	updatedBooks := bookstore.AddBook(books, newBook)
 
 	if len(updatedBooks) != len(books)+1 {
 		t.Errorf("Failed to add book: %+v to books: %+v", newBook, books)
+	}
+}
+
+func TestGetAllBooks(t *testing.T) {
+	want := bookstore.Books
+
+	got := bookstore.GetAllBooks()
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetBookDetails(t *testing.T) {
+	t.Parallel()
+
+	want := "Death by Richard Beliveau - A deep dive into the mysteries of life and the inevitable reality of death.\n"
+
+	got := bookstore.GetBookDetails("Book02")
+
+	if got != want {
+		t.Errorf("Expected: \n%q, but got: \n%q", want, got)
+	}
+}
+
+func TestGetAllBookDetails(t *testing.T) {
+	t.Parallel()
+
+	want := "Spark Joy by Marie Kondo - A tiny, cheerful Japanese woman explains tidying.\n" +
+		"Death by Richard Beliveau - A deep dive into the mysteries of life and the inevitable reality of death.\n" +
+		"Lord of the rings by J.R.R Tolkien - An epic fantasy novel that chronicles the adventures of hobbits, elves, and men against the dark lord Sauron.\n"
+
+	got := bookstore.GetAllBookDetails()
+
+	if want != got {
+		t.Errorf("Expected: \n%q, but got: \n%q", want, got)
 	}
 }
